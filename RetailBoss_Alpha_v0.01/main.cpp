a0f73ff198547d0028e218adb5b188e915d6342f
ppf3 <ppf3@njit.edu>
2014-09-23 15:16:18 -0400

    Got my first image to show by using LazyFoo tutorial

---
+++ RetailBoss_Alpha_v0.01/main.cpp
@@ -0,0 +1,34 @@
+#include "SDL.h"
+
+int main( int argc, char* args[])
+{
+	//The images
+	SDL_Surface* hello = NULL;
+	SDL_Surface* screen = NULL;
+
+	//Start SDL
+	SDL_Init( SDL_INIT_EVERYTHING );
+
+	//Set up screen
+	screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+
+	//Load image
+	hello = SDL_LoadBMP( "images/hello.bmp" );
+
+	//Apply image to screen
+	SDL_BlitSurface( hello, NULL, screen, NULL );
+
+	//Update Screen
+	SDL_Flip( screen );
+
+	//Pause
+	SDL_Delay( 2000 );
+
+	//Free/Remove the loaded image from memory
+	SDL_FreeSurface( hello );
+
+	//Quit SDL
+	SDL_Quit();
+
+	return 0;
+}
